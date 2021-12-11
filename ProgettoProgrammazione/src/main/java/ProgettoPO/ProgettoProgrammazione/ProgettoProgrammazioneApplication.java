ff25ff5531dbf21ff702d6e588125ebc9bf8d57c
Andrea <95300792+AndreaMarini01@users.noreply.github.com>
2021-12-11 13:03:56 +0100

    Add files via upload

---
+++ ProgettoProgrammazione/src/main/java/ProgettoPO/ProgettoProgrammazione/ProgettoProgrammazioneApplication.java
@@ -0,0 +1,13 @@
+package ProgettoPO.ProgettoProgrammazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoProgrammazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoProgrammazioneApplication.class, args);
+	}
+
+}
