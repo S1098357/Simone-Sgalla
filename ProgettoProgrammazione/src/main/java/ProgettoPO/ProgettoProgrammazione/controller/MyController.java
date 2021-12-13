9b338b6f5e6749e2eea38387682de8b0bc336bc8
Andrea <95300792+AndreaMarini01@users.noreply.github.com>
2021-12-13 15:44:07 +0100

    Update MyController.java

--- ProgettoProgrammazione/src/main/java/ProgettoPO/ProgettoProgrammazione/controller/MyController.java
+++ ProgettoProgrammazione/src/main/java/ProgettoPO/ProgettoProgrammazione/controller/MyController.java
@@ -4,7 +4,7 @@ package ProgettoPO.ProgettoProgrammazione.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 import ProgettoPO.ProgettoProgrammazione.entities.Comment;
 import ProgettoPO.ProgettoProgrammazione.services.CommentService;
@@ -20,9 +20,12 @@ public class MyController {
 	//riporta tutti i commenti
 	@GetMapping("/comments")
 	public List<Comment> getComments () {
+	return this.commentService.getComments();
+	}
 	
+	//riporta un singolo commento tramite id
+	@GetMapping("/comments/{id}")
+	public Comment getComment (@PathVariable long commentId) {}
 	
-	
-}
 	
 }
