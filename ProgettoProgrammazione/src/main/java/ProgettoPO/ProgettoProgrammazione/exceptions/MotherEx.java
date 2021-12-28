8218fa1317fb1938b111bbd78573969b43e60ff4
Andrea <95300792+AndreaMarini01@users.noreply.github.com>
2021-12-28 10:19:37 +0100

    Delete ProgettoProgrammazione directory

--- ProgettoProgrammazione/src/main/java/ProgettoPO/ProgettoProgrammazione/exceptions/MotherEx.java
+++
@@ -1,9 +0,0 @@
-package ProgettoPO.ProgettoProgrammazione.exceptions;
-
-public class MotherEx extends Exception {
-	public String errore;
- 
-	public MotherEx (String errore) {};
-	
-	public String getErrore() {return errore;}
-}
